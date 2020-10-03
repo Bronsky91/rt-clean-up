5c9fec26eb6d512b79582b8b82ba6327b34f21de
Ross <ross.srk@gmail.com>
2020-10-02 21:39:52 -0700

    Data Cleanup form WIP

--- pages/data-cleanup.tsx
+++ pages/data-cleanup.tsx
@@ -1,4 +1,5 @@
 import PageLayout from "../layouts/page-layout";
+import styles from "../styles/DataCleanupPage.module.scss";
 import axios from "axios";
 import { useState } from "react";
 import { RedtailContact } from "../interfaces/redtail.interface";
@@ -8,9 +9,32 @@ export default function DataCleanupPage() {
   // TODO: Indicate that the database is being imported and created on the server somehow
 
   const initialFormData = Object.freeze({
-    name: "",
-    advisor: "",
-    writing: "",
+    family_name: "",
+    salutation: "",
+    first_name: "",
+    middle_name: "",
+    last_name: "",
+    nickname: "",
+    gender: "",
+    category: "",
+    status: "",
+    source: "",
+    referred_by: "",
+    servicing_advisor: "",
+    writing_advisor: "",
+    phone_numbers: [{ phone_number: "", type: "", primary: false }],
+    email_addresses: [{ email_address: "", type: "", primary: false }],
+    street_addresses: [
+      {
+        street_address: "",
+        secondary_address: "",
+        city: "",
+        state: "",
+        zip: "",
+        type: "",
+        primary: false,
+      },
+    ],
   });
 
   const [formData, updateFormData] = useState(initialFormData);
@@ -60,49 +84,165 @@ export default function DataCleanupPage() {
       .then((res) => {
         const contact: RedtailContact = res.data[0];
         updateFormData({
-          name: contact.last_name,
-          writing: contact.first_name,
-          advisor: contact.type,
+          family_name: "",
+          salutation: contact.salutation_id
+            ? contact.salutation_id.toString()
+            : "",
+          first_name: contact.first_name,
+          middle_name: contact.middle_name,
+          last_name: contact.last_name,
+          nickname: contact.nickname,
+          gender: contact.gender ? contact.gender.toString() : "",
+          category: contact.category_id ? contact.category_id.toString() : "",
+          status: contact.status_id ? contact.status_id.toString() : "",
+          source: contact.source_id ? contact.source_id.toString() : "",
+          referred_by: contact.referred_by,
+          servicing_advisor: contact.servicing_advisor_id
+            ? contact.servicing_advisor_id.toString()
+            : "",
+          writing_advisor: contact.writing_advisor_id
+            ? contact.writing_advisor_id.toString()
+            : "",
+          phone_numbers: [{ phone_number: "", type: "", primary: false }],
+          email_addresses: [{ email_address: "", type: "", primary: false }],
+          street_addresses: [
+            {
+              street_address: "",
+              secondary_address: "",
+              city: "",
+              state: "",
+              zip: "",
+              type: "",
+              primary: false,
+            },
+          ],
         });
       });
   };
 
+  const toggleFilterModal = (event) => {
+    // TODO
+  };
+
   return (
-    <div>
-      <button onClick={populateList}>Populate Contact List</button>
-      <select onChange={contactSelected} name="contact-list" size={3}>
-        {contactList.map((contact, index) => (
-          <option key={index}>{contact.id}</option>
-        ))}
-      </select>
-      <label>
-        Name:
+    <div className={styles.container}>
+      <div className={styles.pageTitle}>Data Cleanup</div>
+      <div className={styles.contactsPanel}>
+        <div className={styles.contactsTopRow}>
+          <label className={styles.contactsTitle}>Contacts</label>
+          <button className={styles.filterButton} onClick={toggleFilterModal} />
+        </div>
         <input
+          className={styles.contactSearch}
           type="text"
-          name="name"
-          onChange={handleChange}
-          value={formData.name}
+          placeholder="Search.."
         />
-      </label>
-      <label>
-        Advisor:
-        <input
-          type="text"
-          name="advisor"
-          onChange={handleChange}
-          value={formData.advisor}
-        />
-      </label>
-      <label>
-        Writing:
-        <input
-          type="text"
-          name="writing"
-          onChange={handleChange}
-          value={formData.writing}
-        />
-      </label>
-      <input type="submit" value="Update" onClick={handleSubmit} />
+        <select
+          className={styles.contactSelect}
+          onChange={contactSelected}
+          name="contact-list"
+          size={100}
+        >
+          {contactList.map((contact, index) => (
+            <option key={index}>{contact.id}</option>
+          ))}
+        </select>
+      </div>
+      <div className={styles.contactForm}>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Family Name</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="family_name"
+            onChange={handleChange}
+            value={formData.family_name}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Salutation</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="salutation"
+            onChange={handleChange}
+            value={formData.salutation}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>First Name</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="first_name"
+            onChange={handleChange}
+            value={formData.first_name}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Middle Name</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="middle_name"
+            onChange={handleChange}
+            value={formData.middle_name}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Last Name</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="last_name"
+            onChange={handleChange}
+            value={formData.last_name}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Nickname</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="nickname"
+            onChange={handleChange}
+            value={formData.nickname}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Gender</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="gender"
+            onChange={handleChange}
+            value={formData.gender}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Category</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="category"
+            onChange={handleChange}
+            value={formData.category}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Status</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            name="status"
+            onChange={handleChange}
+            value={formData.status}
+          />
+        </div>
+
+        <button onClick={populateList}>Populate Contact List</button>
+        <input type="submit" value="Update" onClick={handleSubmit} />
+      </div>
     </div>
   );
 }
