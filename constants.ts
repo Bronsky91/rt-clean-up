b374166c3f7a8639c677e41d4545a7c52491fc86
Bryan <bryan87reed@gmail.com>
2020-10-22 17:15:30 -0700

    Updates env vars

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || "";
-export const GOOGLE_AUTH_URL = process.env.GOOGLE_AUTH_URL || "";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+export const GOOGLE_AUTH_URL = process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL || "";
