232581d80b235b7349e865b67f6fa986476e37e6
Bryan <bryan87reed@gmail.com>
2020-10-22 17:04:10 -0700

    Updates constants

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || "http://localhost:8000/api";
-export const GOOGLE_AUTH_URL = process.env.GOOGLE_AUTH_URL || "http://localhost:8000/auth/google";
+export const API_URL = process.env.API_URL || "";
+export const GOOGLE_AUTH_URL = process.env.GOOGLE_AUTH_URL || "";
