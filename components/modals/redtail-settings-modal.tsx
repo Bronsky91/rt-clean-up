5e1ff71c6cc96d70dbfa92956d28cfdb79aacdd6
Bryan <bryan87reed@gmail.com>
2020-11-12 14:51:08 -0700

    Adds delay to logout

--- components/modals/redtail-settings-modal.tsx
+++ components/modals/redtail-settings-modal.tsx
@@ -1,12 +1,19 @@
 import Axios from "axios";
+import { useState } from "react";
 import Modal from "react-modal";
 import styles from "../../styles/RedtailModal.module.scss";
+import Loader from "react-loader-spinner";
+import { delay } from "../../utils/delay";
 
 export default function RedtailSettingsModal(props) {
+  const [loadingPage, setLoadingPage] = useState(false);
+
   const redtailLogout = () => {
     Axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users/redtail-logout`, {
       withCredentials: true,
     });
+    // Adding delay so the logout process isn't so quick and jarring to the user
+    return delay(1000);
   };
 
   const handleClose = (e) => {
@@ -14,14 +21,20 @@ export default function RedtailSettingsModal(props) {
   };
 
   const handleSwitchClick = (e) => {
-    redtailLogout();
-    // True is Switching Account
-    props.closeModal(true);
+    setLoadingPage(true);
+    redtailLogout().then(() => {
+      setLoadingPage(false);
+      // True is Switching Account
+      props.closeModal(true);
+    });
   };
 
   const handleLogoutClick = (e) => {
-    redtailLogout();
-    props.closeModal(false);
+    setLoadingPage(true);
+    redtailLogout().then(() => {
+      setLoadingPage(false);
+      props.closeModal(false);
+    });
   };
 
   const customStyles = {
@@ -59,12 +72,25 @@ export default function RedtailSettingsModal(props) {
           <h1 className={styles.title}>Settings</h1>
         </div>
         <div className={styles.inputContainer}>
-          <div className={styles.redButton} onClick={handleSwitchClick}>
-            Switch Account
-          </div>
-          <div className={styles.redButton} onClick={handleLogoutClick}>
-            Log Out
-          </div>
+          {loadingPage ? (
+            <div className={styles.spinner}>
+              <Loader
+                type="Rings"
+                color="#ae3636"
+                height={100}
+                width={100}
+              ></Loader>
+            </div>
+          ) : (
+            <>
+              <div className={styles.redButton} onClick={handleSwitchClick}>
+                Switch Account
+              </div>
+              <div className={styles.redButton} onClick={handleLogoutClick}>
+                Log Out
+              </div>
+            </>
+          )}
         </div>
       </div>
     </Modal>
